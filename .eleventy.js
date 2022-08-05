6a52067c70ea4812280caf50fe48c8984b6c38a1
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-08-05 11:48:19 +0200

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setUseGitIgnore(false);
+
+    eleventyConfig.addPassthroughCopy({'./node_modules/alpinejs/dist/cdn.min.js': './js/alpine.js',})
+    eleventyConfig.addPassthroughCopy("social.png");
+
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+        process.env.ELEVENTY_PRODUCTION &&
+        outputPath &&
+        outputPath.endsWith(".html")
+    ) {
+        let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        });
+        return minified;
+    }
+    return content;
+    })
+
+    return {
+        dir: {
+            input: "src",
+            data: "data",
+            layouts: "layouts",
+            includes: "includes"
+        },
+        htmlTemplateEngine: "njk"
+    };
+};
---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['_site/**/*.html'],
+  safelist: [],
+  darkMode: "media",
+  theme: {
+    colors: {
+      gray: colors.gray
+    },
+    container: {
+        center: true,
+        padding: {
+            DEFAULT: '1rem',
+            sm: '2rem',
+            lg: '4rem',
+            xl: '4rem',
+            '2xl': '4rem',
+            },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
